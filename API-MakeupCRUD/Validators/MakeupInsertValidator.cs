129bcdc1330d03ce2487d65cfa4b550f6a1e82e9
Im Adriana Clemente <clemente.hernandez.alicia.adriana@gmail.com>
2025-04-14 10:11:42 -0600

    Implementacion de validaciones

---
+++ API-MakeupCRUD/Validators/MakeupInsertValidator.cs
@@ -0,0 +1,16 @@
+﻿using API_MakeupCRUD.DTOs;
+using FluentValidation;
+
+namespace API_MakeupCRUD.Validators
+{
+    public class MakeupInsertValidator: AbstractValidator<MakeupInsertDto>
+    {
+        public MakeupInsertValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is obligatory");
+            RuleFor(x => x.BrandID).NotNull().WithMessage( x => "Brand is obligatory");
+            RuleFor(x => x.BrandID).GreaterThan(0).WithMessage(x => "Error with the value sent");
+            RuleFor(x => x.Volume).GreaterThan(0).WithMessage(x => "{PropertyName} must be greater than 0");
+        }
+    }
+}
---
+++ API-MakeupCRUD/Validators/MakeupUpdateValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using API_MakeupCRUD.DTOs;
+
+namespace API_MakeupCRUD.Validators
+{
+    public class MakeupUpdateValidator: AbstractValidator<MakeupUpdateDto>
+    {
+        public MakeupUpdateValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty().WithMessage("Id is obligatory");
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is obligatory");
+            RuleFor(x => x.BrandID).NotNull().WithMessage(x => "Brand is obligatory");
+            RuleFor(x => x.BrandID).GreaterThan(0).WithMessage(x => "Error with the value sent");
+            RuleFor(x => x.Volume).GreaterThan(0).WithMessage(x => "{PropertyName} must be greater than 0");
+        }
+    }
+}
