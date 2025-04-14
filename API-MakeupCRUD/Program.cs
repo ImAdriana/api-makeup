3c96711b1e9a892dfe0996fd52f3e0f8874a4151
Im Adriana Clemente <clemente.hernandez.alicia.adriana@gmail.com>
2025-04-14 10:12:17 -0600

    Inyección de dependencias

--- API-MakeupCRUD/Program.cs
+++ API-MakeupCRUD/Program.cs
@@ -1,17 +1,25 @@
 using API_MakeupCRUD.Context;
+using API_MakeupCRUD.DTOs;
+using API_MakeupCRUD.Validators;
+using FluentValidation;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Entity Framework
 var connectionString = builder.Configuration.GetConnectionString("Connection");
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
 
+// Validator
+builder.Services.AddScoped<IValidator<MakeupInsertDto>, MakeupInsertValidator>();
+builder.Services.AddScoped<IValidator<MakeupUpdateDto>, MakeupUpdateValidator>();
 builder.Services.AddControllers();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
