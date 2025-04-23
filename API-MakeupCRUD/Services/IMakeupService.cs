059fddb96173cef4358b43a70b27a510de0783f2
Im Adriana Clemente <clemente.hernandez.alicia.adriana@gmail.com>
2025-04-23 13:24:26 -0600

    Implementación de la capa de servicio

---
+++ API-MakeupCRUD/Services/IMakeupService.cs
@@ -0,0 +1,14 @@
+﻿using API_MakeupCRUD.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_MakeupCRUD.Services
+{
+    public interface IMakeupService
+    {
+        Task<IEnumerable<MakeupDto>> Get();
+        Task<MakeupDto> GetById(int id);
+        Task<MakeupDto> Update(int id, MakeupUpdateDto makeupUpdatedDto);
+        Task<MakeupDto> Add(MakeupInsertDto makeupInsertDto);
+        Task<MakeupDto> Delete(int id);
+    }
+}
---
+++ API-MakeupCRUD/Services/MakeupService.cs
@@ -0,0 +1,133 @@
+﻿using API_MakeupCRUD.Context;
+using API_MakeupCRUD.DTOs;
+using API_MakeupCRUD.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_MakeupCRUD.Services
+{
+    public class MakeupService : IMakeupService
+    {
+        private AppDbContext _context;
+
+        public MakeupService(AppDbContext context)
+        {
+            _context = context;
+            
+        }
+        public async Task<IEnumerable<MakeupDto>> Get() =>
+            await _context.MakeupProducts.Select(m => new MakeupDto
+            {
+                Id = m.Id,
+                Name = m.Name,
+                BrandID = m.BrandID,
+                Volume = m.Volume,
+                Price = m.Price,
+                Type = m.Type,
+            }).ToListAsync();
+        
+           
+        
+
+        public async Task<MakeupDto> GetById(int id)
+        {
+            var makeup = await _context.MakeupProducts.FindAsync(id);
+            if (makeup != null)
+            {
+                var makeupDto = new MakeupDto
+                {
+                    Id = makeup.Id,
+                    Name = makeup.Name,
+                    BrandID = makeup.BrandID,
+                    Volume = makeup.Volume,
+                    Price = makeup.Price,
+                    Type = makeup.Type,
+                };
+                return makeupDto;
+            }
+            return null;
+
+
+        }
+        public async Task<MakeupDto> Add(MakeupInsertDto makeupInsertDto)
+        {
+            var makeup = new MakeupProduct()
+            {
+                Name = makeupInsertDto.Name,
+                BrandID = makeupInsertDto.BrandID,
+                Volume = makeupInsertDto.Volume,
+                Price = makeupInsertDto.Price,
+                Type = makeupInsertDto.Type,
+            };
+
+            await _context.MakeupProducts.AddAsync(makeup);
+            await _context.SaveChangesAsync(); // La base de datos representa los cambios
+
+            var makeupDto = new MakeupDto
+            {
+                Id = makeup.Id,
+                Name = makeup.Name,
+                BrandID = makeup.BrandID,
+                Volume = makeup.Volume,
+                Price = makeup.Price,
+                Type = makeup.Type,
+            };
+            return makeupDto;
+        }
+
+        public async Task<MakeupDto> Delete(int id)
+        {
+            var makeup = await _context.MakeupProducts.FindAsync(id);
+
+            if (makeup != null)
+            {
+                var makeupDto = new MakeupDto
+                {
+                    Id = makeup.Id,
+                    Name = makeup.Name,
+                    BrandID = makeup.BrandID,
+                    Volume = makeup.Volume,
+                    Price = makeup.Price,
+                    Type = makeup.Type,
+                };
+                _context.Remove(makeup);
+                await _context.SaveChangesAsync();
+
+                return makeupDto;
+            }
+            return null;
+        }
+
+        
+
+        public async Task<MakeupDto> Update(int id, MakeupUpdateDto makeupUpdatedDto)
+        {
+            var makeup = await _context.MakeupProducts.FindAsync(id);
+
+            if (makeup != null)
+            {
+                makeup.Name = makeupUpdatedDto.Name;
+                makeup.Volume = makeupUpdatedDto.Volume;
+                makeup.Price = makeupUpdatedDto.Price;
+                makeup.Type = makeupUpdatedDto.Type;
+                makeup.Id = makeupUpdatedDto.Id;
+
+                await _context.SaveChangesAsync();
+
+                var makeupDto = new MakeupDto
+                {
+                    Id = makeup.Id,
+                    Name = makeup.Name,
+                    BrandID = makeup.BrandID,
+                    Volume = makeup.Volume,
+                    Price = makeup.Price,
+                    Type = makeup.Type,
+                };
+                return makeupDto;
+            }
+            return null;
+
+            
+        }
+    }
+}
